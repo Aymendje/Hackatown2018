db6f3393534586e5640e8d99feb4e1990128531e
Yonni Chen <yonni.scholars@gmail.com>
2018-01-20 18:14:04 -0500

    Tiles for results

--- client/src/app/pages/daycare/daycare.component.ts
+++ client/src/app/pages/daycare/daycare.component.ts
@@ -14,14 +14,38 @@ export class DaycareComponent implements OnInit {
   public prix: number;
   public isPublic: boolean;
 
+  public daycares: Array<any>;
+
   ngOnInit() {
+    this.initialize();
+  }
+
+  private initialize() {
     this.distance = 5;
     this.nombreEnfants = 1;
     this.prix = 8;
     this.isPublic = true;
+
+    this.daycares = [
+      {
+        name: "Garderie du bonheur",
+        distance: 5,
+        price: 12,
+        description: "Situé proche d'une école primaire et facilement accessible via l'autoroute 20, la Garderie du bonheur est la garderie parfaite pour les budgets moyens",
+        available: 20
+      },
+      {
+        name: "Garderie du Soleil",
+        distance: 7.5,
+        price: 9,
+        description: "Situé proche d'une école primaire et facilement accessible via l'autoroute 20, la Garderie du bonheur est la garderie parfaite pour les budgets moyens",
+        available: 6
+      }
+    ];
   }
 
   public query() {
     // Calling the web service
+    alert("query");
   }
 }
